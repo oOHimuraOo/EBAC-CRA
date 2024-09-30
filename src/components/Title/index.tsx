d05bc91fd2bfdb61803c26a814b65c2a476f3566
Felipe Cerqueira <felipecer06@gmail.com>
2024-09-30 19:49:58 -0300

    CRA CSS in JS

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,11 @@
+import { Titulo as TituloEstilo } from './styles'
+
+export type Props = {
+  children: string;
+  fontSize?: number;
+  inBox?: boolean;
+}
+
+const Titulo = (props:Props) => <TituloEstilo fontSize={props.fontSize} inBox={props.inBox}>{props.children}</TituloEstilo>
+
+export default Titulo
