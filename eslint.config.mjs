f715d7ea1473900b4e1cba9dfe1c7b3aca5e10f8
Felipe Cerqueira <felipecer06@gmail.com>
2024-09-30 13:46:52 -0300

    descobrindo como as ferramentas funcionam

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,33 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import pluginPrettier from 'eslint-plugin-prettier'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    plugins: [
-      "react-hooks"
-    ],
-    rules: {
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/explicit-module-boudary-types": "off",
+    plugins: {
+      react_hooks: reactHooks,
+      plugin_prettier: pluginPrettier
     },
-    setings: {
+    rules: {
+      'react_hooks/rules-of-hooks': 'error',
+      'react_hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boudary-types': 'off',
+      'plugin_prettier/prettier': 'error'
+    },
+    settings: {
       react: {
-        version: "detect"
+        version: 'detect'
       }
     }
   }
-];
+]
