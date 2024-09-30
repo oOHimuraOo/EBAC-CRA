d489e66af5325f0883cf9b82193862c578780dfb
Felipe Cerqueira <felipecer06@gmail.com>
2024-09-30 10:53:32 -0300

    commit de inicial

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: [
+      "react-hooks"
+    ],
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boudary-types": "off",
+    },
+    setings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
+];
